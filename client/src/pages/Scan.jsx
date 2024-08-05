860085b26216f551cb67340c9d30c0829fe8a7a3
i-am-a-shish <simplyashish1030@gmail.com>
2024-08-05 13:24:56 +0530

    scan page

--- client/src/pages/Scan.jsx
+++ client/src/pages/Scan.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { IoPersonSharp } from "react-icons/io5";
+import "./scan.css"
 
 const Scan = () => {
-  return (
-    <div>Scan</div>
-  )
+  return <>
+    <div className="main-container">
+    <div className="container">
+        <div className="loader"><IoPersonSharp size={150}/>
+        <span id="countdown"> 0</span></div>
+    </div>
+    <p id="statusMessage">You are now at position: <span id="statusNumber">0</span></p>
+    <div className="button-container">
+        <button className="btn" id="toggle-button">Book Now</button>
+    </div>
+</div>
+
+</>
 }
 
 export default Scan
