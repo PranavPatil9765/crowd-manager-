50e0c30071b48306077d851996dabd6c26ea7a9f
unknown <pranavpatil9765@gmail.com>
2024-08-10 19:28:54 +0530

    product page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import './App.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Product from './pages/Product'
 import Home from './pages/Home'
 import Scan from './pages/Scan'
 import Navbar from './components/navbar.jsx'
 const App = () => {
   return <>
-    <BrowserRouter>
+  
       <Navbar/>
+
       <Routes>
         <Route path='/' element = {<Home/>} />
+        <Route path='/:product' element = {<Product/>} />
         <Route path='/scan' element = {<Scan/>} />
       </Routes>
-    </BrowserRouter>
+  
   </>
 }
 
